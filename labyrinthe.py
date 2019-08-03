6ba815baccd4ecbc1751fb72735f304fe1c48b27
bbessayah@gmail.com <Berel@x151>
2019-08-03 18:27:08 +0200

    All in english version

--- labyrinthe.py
+++ labyrinthe.py
@@ -17,7 +17,6 @@ import pygame.display
 from pygame.locals import *
 pygame.init()
 
-
 #### VARIABLES ####
 # x y initialisation
 x = 0
@@ -45,44 +44,43 @@ ETHER = 'N'
 NEEDLE = 'O'
 TUBE = 'P'
 
-# concordance variables - lettres fichier texte
+# beginning, ending, wall regarding letters of file
 START = 'D'
 OUT = 'S'
 WALL = 'M'
 
 
-#### CLASSES ####
-### classe permettant de représenter le labyrinthe
+#### CLASS ####
+
+# The Labyrinth class will contain all the information to create the structure of the labyrinth
 class Labyrinthe():
     def __init__(self, fichier_laby):
+        # x and y will contain the coordinates of the letters
         self.x = 0
         self.y = 0
+        # list_all will be a list of list who contain the letters of the structure of the labyrinth
         self.list_all = self.lec_fichier(fichier_laby)
+        # remaining_objects will permit to check items always in the labyrinth
         self.remaining_objects = {ETHER, NEEDLE, TUBE}
+        # list to if case is an object
         self.list_object = ['N', 'O', 'P']
-        #self.list_position_mcgyver = self.position_mcgyver(x, y)
         self.dimension = 15
 
     def lec_fichier(self, a):
         """ To read labyrinthe file """
         self.liste_all = []
-        # si relance de la partie affichage de l'annonce
         with open(a, "r") as fichier:
-            # découpage des lignes dans la variable lecture
+            # cut file by line
             reading = fichier.read().splitlines()
-            # création d'un tableau, via la  liste_letter dans la liste_all
-
+            # cut line by letter to creating a table 2 dimensions
             for line in reading:
-                # découpe les caracteres de chaque ligne
-                # intégration de ces caractères dans la liste liste_letter
                 liste_letter = [i for i in line]
-                # intégration de chaque liste dans la liste liste_all
                 self.liste_all += [liste_letter]
             return(self.liste_all)
 
 
     def position(self, a, b):
-        """ fontion permettant de selectionner une position selon les axes y et x """
+        """ to find a position in labyrinth regading x & y """
         position = self.list_all[b][a]
         return position
 
@@ -98,11 +96,11 @@ class Labyrinthe():
             # select with random 1 position
             prev = self.position(axe1, axe2)
             if prev == 'C':
-                # if C, we change the letter of this position in liste_all by one of 3 letter
+                # if C, we change the letter of this position in list_all by one of 3 letter
                 self.list_all[axe2][axe1] = x.pop()
 
     def mouvement(self, touch, x, y):
-        # to change x & y variables
+        """ to update x & y variables """
         if touch == K_DOWN:
             # if down, y takes +1
             if 0 <= y <= 13:
@@ -125,66 +123,54 @@ class Labyrinthe():
         """ check objects took """
         if (e == ETHER or e == NEEDLE or e == TUBE):
             """ player comes to a case, if the case = 1 of 3 objects """
-            #print("C'est un objet")
             self.remaining_objects.remove(e)
-            #self.list_object(e)
             print(self.remaining_objects)
 
-    def macgyver_visual(self, y2, x2):
-        show_laby = deepcopy(a.list_all)
-        show_laby[y2][x2] = "X"
-        for r in show_laby:
-            print(r)
-
-
 #### FONCTIONS ####
-def annonce(nom):
-    """ Print the announce """
-    print("*** Hello " + nom + ", bienvenue dans le nouveau, super, fabuleux jeux de Mac Gyver!!! ***")
-    print("Tu dois aider Mac Gyver (Big Mac pour les intimes) à sortir!")
-    print("Tu devras recupérer tous les objets pour créer une")
-    print("seringue et endormir le gardien à la sortie ;) .")
-    print("Les touches directions sont e : Haut, x : bas, d : droite et s :gauche")
-    print("Pour quitter, appuyer sur la touche q")
 
 def pyg_laby():
+    """ to create visual labyrinth structure"""
     laby_fenetre.blit(laby_background, (0, 0))
     laby_mini_background.blit(laby_mini_background, (0, SCREEN))
     x_lab = 0
     y_lab = 0
     while x_lab <= 14:
         while y_lab <= 14:
+            # to create a wall
             if a.liste_all[y_lab][x_lab] == 'M':
-                #print(str(x_lab) + " * " + str(y_lab))
                 laby_fenetre.blit(mur, (x_lab * CASE_SIZE, y_lab * CASE_SIZE))
+            # to create an ether
             elif a.liste_all[y_lab][x_lab] == ETHER:
-                #print(str(x_lab) + " * " + str(y_lab))
                 laby_fenetre.blit(img_ether, (x_lab * CASE_SIZE, y_lab * CASE_SIZE))
+            # to create an needle
             elif a.liste_all[y_lab][x_lab] == NEEDLE:
-                #print(str(x_lab) + " * " + str(y_lab))
                 laby_fenetre.blit(img_aiguille, (x_lab * CASE_SIZE, y_lab * CASE_SIZE))
+            # to create a tube
             elif a.liste_all[y_lab][x_lab] == TUBE:
-                #print(str(x_lab) + " * " + str(y_lab))
                 laby_fenetre.blit(img_tube_plastique, (x_lab * CASE_SIZE, y_lab * CASE_SIZE))
+            # to create the gardian
             elif a.liste_all[y_lab][x_lab] == 'S':
                 #print(str(x_lab) + " * " + str(y_lab))
                 laby_fenetre.blit(mechant, (x_lab * CASE_SIZE, y_lab * CASE_SIZE))
             y_lab += 1
+        # y must be reset for the next x
         y_lab = 0
         x_lab += 1
     x_lab = 0
 
-def compt_obj(comp):
-    for azer in comp:
-        if azer == NEEDLE:
-            print("Yes")
-            laby_fenetre.blit(img_mini_tube_plastique, (CASE_SIZE, SCREEN))
-
-
-        #f i =! TUBE:
-         #   laby_fenetre.blit(img_mini_tube_plastique, (CASE_SIZE * 2, SCREEN))
+def compt_obj():
+    """ to print object if always in the labyrinth """
+    if NEEDLE in a.remaining_objects:
+        laby_fenetre.blit(img_mini_aiguille, (CASE_SIZE, SCREEN))
+    if ETHER in a.remaining_objects:
+        laby_fenetre.blit(img_mini_ether, (CASE_SIZE * 2, SCREEN))
+    if TUBE in a.remaining_objects:
+        laby_fenetre.blit(img_mini_tube_plastique, (CASE_SIZE * 3, SCREEN))
+    if len(a.remaining_objects) == 0:
+        laby_fenetre.blit(img_seringue, (CASE_SIZE * 4, SCREEN))
 
 def get_true_filename(filename):
+    """ function to permit to start the program in windows, linux or mac OS """
     try:
         # Hack for pyInstaller. Refer https://stackoverflow.com/a/13790741
         base = sys._MEIPASS
@@ -192,9 +178,10 @@ def get_true_filename(filename):
         base = os.path.abspath(".")
     return os.path.join(base, filename)
 
+
 if __name__=='__main__':
-    #print(CASE_SIZE)
-    # pygame variable
+
+    # pygame variable to print at the beginning
     laby_fenetre = pygame.display.set_mode((SCREEN, SCREEN + CASE_SIZE))
     laby_background = pygame.image.load(get_true_filename("background_test.jpg")).convert()
     laby_background = pygame.transform.scale(laby_background, (SCREEN, SCREEN))
@@ -244,35 +231,30 @@ if __name__=='__main__':
 
     pygame.display.flip()
 
-    # control that is a first play
-
-    # Creating of 'a' object
+    # creating of 'a' object
     a = Labyrinthe(get_true_filename("labyrinth.txt"))
 
+    # to put the objects in the labyrinth
     a.pos_object(a.list_object)
 
-        #pygame.display.flip()
-
-    #joueur = input("Quel est votre nom de joueur?\n")
-    #annonce(joueur)
-
+    # driving the user faster
     pygame.key.set_repeat(400, 30)
 
+    # main loop
     while BIG_MAC != OUT:
-        rip_check = 0
+        # to initiate winner or looser variable
         win_check = 0
         for event in pygame.event.get():
+            # if player click on cross we close the program
             if event.type == QUIT:
                 BIG_MAC = OUT
             if event.type == KEYDOWN:
-
                 x2, y2 = a.mouvement(event.key, x, y)
+                # if new position is a wall, we do nothing
                 if a.position(x2, y2) == WALL:
                     x2 = x2
-                    #print("C'est un mur!!! ")  # + "*" +action + "*" +str(x) + " " + str(y) + " " + str(big_mac))
                 else:
-                    # on MAJ les coordonnées de la variable big_mac
-                    #print(x2)
+                    # to change player position on the screen
                     if event.key == K_DOWN and y < 14:
                         position_mcg = position_mcg.move(0, CASE_SIZE)
                     if event.key == K_LEFT and x > 0:
@@ -284,37 +266,30 @@ if __name__=='__main__':
                     x = x2
                     y = y2
                     BIG_MAC = a.position(x, y)
-                    #print(position_mcg)
 
-                        # fonction pour le controle des objets
+                    # to check if the player position is on an object
                     a.saisie_objet(BIG_MAC)
-                        # modification de la lettre actuelle en C (apres verif mur et objets)
+                    # to change the visual of the position for the next loop (it will be a way)
                     a.list_all[y2][x2] = 'C'
-                    compt_obj(a.remaining_objects)
-                    #compt_obj()
-
+                # if the player didn't take all objects
                 if BIG_MAC == OUT and len(a.remaining_objects) != 0:
-                    #print("Tu n'avais pas tous les objets, RIP Mac Gyver!")
-                    rip_check = 1
-                elif BIG_MAC == OUT and len(a.remaining_objects) == 0:
-                    #print("Bravo , Mac Gyver a endormi le gardin, il est libre!!!")
                     win_check = 1
+                # if the player take all objects
+                elif BIG_MAC == OUT and len(a.remaining_objects) == 0:
+                    win_check = 2
+            # to print the labyrinth
             pyg_laby()
+            # to print the bottom window
             laby_fenetre.blit(laby_mini_background, (0, SCREEN))
+            # to print the new player's position
             laby_fenetre.blit(mcg, position_mcg)
-            if NEEDLE in a.remaining_objects:
-                laby_fenetre.blit(img_mini_aiguille, (CASE_SIZE, SCREEN))
-            if ETHER in a.remaining_objects:
-                laby_fenetre.blit(img_mini_ether, (CASE_SIZE*2, SCREEN))
-            if TUBE in a.remaining_objects:
-                laby_fenetre.blit(img_mini_tube_plastique, (CASE_SIZE*3, SCREEN))
-            if len(a.remaining_objects) == 0:
-                laby_fenetre.blit(img_seringue, (CASE_SIZE*4, SCREEN))
-        if rip_check == 1:
+            # to print the objects always in the Labyrinth
+            compt_obj()
+        if win_check == 1:
             laby_fenetre.blit(rip, (0,0))
-        elif win_check == 1:
+        elif win_check == 2:
             laby_fenetre.blit(mg_win, (0, 0))
         pygame.display.flip()
-    pygame.time.wait(5000)
+    pygame.time.wait(3000)
     pygame.quit()
 
